a154a077cd2c24a97c9b70bc6da279e283406e71
01474985203 <01474985203@JIPA-LABIN05-16.ifro.local>
2018-05-04 15:11:03 -0400

    Aula 7 - Criar tela de cadastro e menu da tela principal

--- Grupo5/src/br/edu/ifro/MenuPrincipalController.java
+++ Grupo5/src/br/edu/ifro/MenuPrincipalController.java
@@ -10,7 +10,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
@@ -24,6 +29,9 @@ public class MenuPrincipalController implements Initializable {
  
     @FXML
     private Button btnSoma;
+   
+    @FXML
+    private Label label;
     
     @FXML
     private void Soma (ActionEvent event) {
@@ -40,5 +48,26 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+   try{
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),900,682);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar Aluno");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    
+    }
     
 }
